5c611c99645ac2aa493a0bfb0e5a08e39067c923
Zitael <45496964+Zitael@users.noreply.github.com>
2019-09-08 14:33:41 +0300

    initial commit

---
+++ src/main/java/ru/alexandrov/currencychecker/CurrencycheckerApplication.java
@@ -0,0 +1,13 @@
+package ru.alexandrov.currencychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencycheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencycheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alexandrov/currencychecker/CurrencycheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.alexandrov.currencychecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencycheckerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
