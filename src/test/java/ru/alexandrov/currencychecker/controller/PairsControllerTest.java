fa13bac2398e5f56f10e28c1b0b440c4f5d2ff62
Zitael <45496964+Zitael@users.noreply.github.com>
2019-09-13 14:41:26 +0300

    still try to make tests...

--- src/test/java/ru/alexandrov/currencychecker/controller/PairsControllerTest.java
+++ src/test/java/ru/alexandrov/currencychecker/controller/PairsControllerTest.java
@@ -1,48 +1,80 @@
 package ru.alexandrov.currencychecker.controller;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import ru.alexandrov.currencychecker.dao.model.PairsModel;
 import ru.alexandrov.currencychecker.service.PairsService;
+import ru.alexandrov.currencychecker.service.PairsServiceImpl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class PairsControllerTest {
-    private MockMvc mockMvc;
-
     @Mock
     private PairsService service;
-
     @InjectMocks
     private PairsController controller;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @Before
     public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
         PairsModel model1 = new PairsModel();
         model1.setCurrency("test1");
-        model1.setLastPrice(2);
-        when(service.saveToDB("test1", 2)).thenReturn(model1);
+        model1.setLastPrice(1);
+        PairsModel model2 = new PairsModel();
+        model2.setCurrency("test1");
+        model2.setLastPrice(2);
+        List<PairsModel> list = new ArrayList<>();
+        list.add(model1);
+        list.add(model2);
+        when(service.saveToDB("test1", 1)).thenReturn(model1);
+        when(service.getLastN("test1", 2)).thenReturn(list);
+        when(service.getFilteredByDelta("test1", 1)).thenReturn(Collections.singletonList(model2));
+    }
+
+    @Test
+    public void testInsertNote() throws Exception {
+        //controller.putToBase(1, "test1");
+        mockMvc.perform(get("/pairs/test1/newNote?price=1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{status : 1}"))
+                .andExpect(status().isOk());
+//        verify(service, times(1)).saveToDB("test1", 1);
     }
 
     @Test
     public void getLast() throws Exception {
-        mockMvc.perform(get("/test1/lastNotes?n=2")
+        mockMvc.perform(get("/pairs/test1/lastNotes?n=2")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+//        verify(service).getLastN("test1", 2);
     }
 
     @Test
@@ -54,4 +86,4 @@ public class PairsControllerTest {
         mockMvc.perform(get("/test1/newNote?price=2"))
                 .andExpect(status().isOk());
     }
-    }
+}
--- src/test/java/ru/alexandrov/currencychecker/service/PairsServiceImplTest.java
+++ src/test/java/ru/alexandrov/currencychecker/service/PairsServiceImplTest.java
@@ -2,17 +2,56 @@ package ru.alexandrov.currencychecker.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.alexandrov.currencychecker.dao.model.PairsModel;
+import ru.alexandrov.currencychecker.dao.repository.PairsRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class PairsServiceImplTest {
+    @Mock
+    private PairsRepository repository;
+    @Autowired
+    private PairsService service;
 
     @Before
     public void setUp() throws Exception {
+        PairsModel model1 = new PairsModel();
+        model1.setLastPrice(1);
+        PairsModel model2 = new PairsModel();
+        model1.setLastPrice(2);
+        PairsModel model3 = new PairsModel();
+        model1.setLastPrice(3);
+        List<PairsModel> list1 = new ArrayList<>();
+        list1.add(model1);
+        list1.add(model2);
+        List<PairsModel> list2 = new ArrayList<>();
+        list2.add(model3);
+
+        when(repository.findAllByCurrency("1", new PageRequest(0, 1, Sort.Direction.DESC, "id")))
+                .thenReturn((Page) list1);
+        when(repository.findFirstByCurrencyOrderByIdDesc("2")).thenReturn(model1);
+        when(repository.findAllByCurrencyAndDeltaAfter("1", 2)).thenReturn(list2);
     }
 
     @Test
     public void getLastN() {
+        List result = service.getLastN("1", 1);
+        assertEquals(2, result.size());
     }
 
     @Test
@@ -21,5 +60,6 @@ public class PairsServiceImplTest {
 
     @Test
     public void saveToDB() {
+
     }
 }
